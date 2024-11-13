33c83d0646668fc93c962a9affc9172dd9446242
sambeep <samsonkinyanjui11@gmail.com>
2024-11-13 14:47:33 +0300

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 import sys
 
+def print_solution(board, n):
+    """Print a solution in the required format"""
+    solution = []
+    for row in range(n):
+        solution.append([row, board[row]])
+    print(solution)
+
 def is_safe(board, row, col):
-    """Check if a queen can be placed at (row, col)"""
+    """Check if it's safe to place a queen at board[row][col]"""
     for i in range(row):
         if board[i] == col or \
            board[i] - i == col - row or \
@@ -10,15 +17,15 @@ def is_safe(board, row, col):
             return False
     return True
 
-def solve_nqueens(n, row, board, solutions):
-    """Use backtracking to find all solutions to the N-Queens problem"""
+def solve_nqueens(n, row, board):
+    """Solve the N Queens problem using backtracking"""
     if row == n:
-        solutions.append([[i, board[i]] for i in range(n)])
-    else:
-        for col in range(n):
-            if is_safe(board, row, col):
-                board[row] = col
-                solve_nqueens(n, row + 1, board, solutions)
+        print_solution(board, n)
+        return
+    for col in range(n):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens(n, row + 1, board)
 
 def main():
     if len(sys.argv) != 2:
@@ -35,12 +42,8 @@ def main():
         print("N must be at least 4")
         sys.exit(1)
     
-    solutions = []
     board = [-1] * n
-    solve_nqueens(n, 0, board, solutions)
-    
-    for solution in solutions:
-        print(solution)
+    solve_nqueens(n, 0, board)
 
 if __name__ == "__main__":
     main()
